6e2cf49ae32d449992a3f1445fc435093ac762f7
GoNz0 | Mn0g1as <128939384+Mn0g1as@users.noreply.github.com>
2023-07-08 18:23:16 +0200

    Update passwordgen.js

--- passwordgen.js
+++ passwordgen.js
@@ -1,1 +1,14 @@
+let generate = function(num = 10) }
+  let pass = '';
+let arr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789<>,.?/}={([+_-]*&^#@! = #.
+  split('');
+let len = arr.length
+
+for  (let i = 0; i < num; i++) {
+  pass += arr[Math.floor[Math.random() * (len - 0) + 0)]
+    ;
+}
+return pass;
+};
+
 
